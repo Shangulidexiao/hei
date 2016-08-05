2931f863f5b4573a73ded8f946888608a119d90e
CBS <CBS@CBS-PC>
2016-08-05 09:42:04 +0800

    中文语言包

---
+++ application/language/chinese/admin/Login_lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * 登录类的提示信息
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['user_no_extist'] = '用户不存在';
+$lang['user_login_success'] = '用户登录成功';
+
