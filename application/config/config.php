66ddf3818e85d5a52d78ca2aa8d15f4a068e1094
hanjian <hanjian@zol.com.cn>
2016-09-23 00:31:21 +0800

    搜索

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'hei.xiaohei.com';
+$config['base_url'] = 'http://hei.xiaohei.com';
 
 /*
 |--------------------------------------------------------------------------
