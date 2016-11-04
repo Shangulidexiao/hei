2123093a8704d6a4514caf696cbb32a7fe6046ea
Administrator <Administrator@SC-201610071407>
2016-11-05 01:21:36 +0800

    菜单真正的完成了

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -39,38 +39,47 @@ class MY_Controller extends CI_Controller {
         $menuFirst = array();
         $menuSecond = array();
         $menuThird = array();
+        #生成顶级菜单
         foreach ($menu as $k1 => $v1) {
             if($v1['parent_id'] === '0'){
                 $menuFirst[] = $v1;
                 unset($menu[$k1]);
             }
         }
-        
+        #生成二级菜单
         foreach ($menuFirst as $k2 => $v2) {
             foreach ($menu as $k3 => $v3) {
                 if($v3['parent_id'] === $v2['id']){
-                    $menuSecond[] = $v3;
+                    $menuSecond[$v2['id']][] = $v3;
                     unset($menu[$k3]);
                 }
             }
         }
-
-        $menuArr = array();
-        $subMenuArr = array();
+        #生成三级菜单
         foreach ($menuSecond as $k4 => $v4) {
-            foreach ($menu as $k5 => $v5) {
-                if($v5['parent_id'] === $v4['id']){
-                    $menuThird[] = $v5;
-                    $subMenuArr[$k5]['id'] = $v5['id'];
-                    $subMenuArr[$k5]['text'] = $v5['name'];
-                    $subMenuArr[$k5]['href'] = $v5['url'];
-                    unset($menu[$k5]);
+            $menuSecondArr = array();#二级菜单 临时数组格式
+            foreach ($v4 as $k5 => $v5) {
+                $subMenuArr = array();#三级菜单 临时数组格式
+                foreach ($menu as $k6 => $v6) {
+                    if($v6['parent_id'] === $v5['id']){
+                        $menuThird[] = $v6;
+                        $subMenuArr[] = array(
+                            'id'    =>  $v6['id'],      #页面唯一标识
+                            'text'  => $v6['name'],     #三级菜单名字
+                            'href'  => $v6['url']       #三级菜单链接
+                        );
+                        unset($menu[$k6]);
+                    }
                 }
+                $menuSecondArr[] = array('text'=>$v5['name'],'items'=>$subMenuArr);
             }
-            $menuArr[$k4]['id'] = $v4['id'];
-            $menuArr[$k4]['menu'][] = array('text'=>$v4['name'],'items'=>array_values($subMenuArr));
+            
+            #菜单数组
+            $menuArr[] = array(
+                'id'    => $k4,             #一级菜单的id 页面的唯一标识
+                'menu'  => $menuSecondArr   #一级菜单下的菜单数组
+            );
         }
-        
         $this->load->vars(array('menuFirst'=>$menuFirst));
         $this->load->vars(array('menuJson'=>json_encode($menuArr)));
     }
