af9b40bde63113ffdfb74f515a10a80be66bb1bb
18335 <18335@DESKTOP-8K2DPNM>
2016-11-11 18:32:53 +0800

    全选全不选

--- application/views/role/adminList.php
+++ application/views/role/adminList.php
@@ -1,11 +1,13 @@
 <form id="J_Form" action="" class="form-horizontal">
     <div class="control-group">
-    <div class="control-label"><s>*</s>人员列表：</div>
-	<div >全选<input type="checkbox"></div>
+    <label class="control-label"><s>*</s>人员列表：</label>
+	<div class="row">
+		<span><input class="select-all" type="checkbox" for-name="admin[]">全选/全不选</span>
+	</div>
     <hr>
     <div class="controls bui-form-group"  data-rules="{checkRange:1}" data-messages="{checkRange:'至少勾选一项！'}" >
       <?php foreach ( $admins as $admin) { ?>
-        <label class="checkbox" for=""><input name="admin[]" value="<?=$admin['id']?>" type="checkbox"><?=$admin['user_name']?></label>&nbsp;&nbsp;&nbsp;
+        <label class="checkbox" for=""><input class="select-one" name="admin[]" value="<?=$admin['id']?>" type="checkbox"><?=$admin['user_name']?></label>&nbsp;&nbsp;&nbsp;
       <?php } ?>
     </div>
     </div>      
--- public/js/hei/common.js
+++ public/js/hei/common.js
@@ -1,3 +1,24 @@
 $(function(){
-	
+    $(document).on('change','.select-all',function(){
+        $this 			= $(this);
+        $isChecked 		= $this.attr('checked');
+        $inputName 		= $this.attr('for-name');
+        $selectInputs           = $("input[name='"+$inputName+"']");
+
+        if($isChecked === 'checked'){
+                $selectInputs.attr('checked','checked');
+        }else{
+                $selectInputs.removeAttr('checked');
+        }
+    });
+    $(document).on('change','.select-one',function(){
+        $selectAll                  = $('.select-all');
+        $checkedInputsNum           = $(".select-one:checked").length;
+        $allInputsNum               = $(".select-one").length;
+        if($checkedInputsNum === $allInputsNum){
+                $selectAll.attr('checked','checked');
+        }else{
+                $selectAll.removeAttr('checked');
+        }
+    }); 
 });
