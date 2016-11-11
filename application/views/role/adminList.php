b6bd862a9fa38aee2a0b8d1836c228f9e86cdad8
18335 <18335@DESKTOP-8K2DPNM>
2016-11-11 17:25:34 +0800

    11

--- application/views/role/adminList.php
+++ application/views/role/adminList.php
@@ -1,6 +1,8 @@
 <form id="J_Form" action="" class="form-horizontal">
     <div class="control-group">
-    <label class="control-label"><s>*</s>人员列表：</label><br>
+    <div class="control-label"><s>*</s>人员列表：</div>
+	<div >全选<input type="checkbox"></div>
+    <hr>
     <div class="controls bui-form-group"  data-rules="{checkRange:1}" data-messages="{checkRange:'至少勾选一项！'}" >
       <?php foreach ( $admins as $admin) { ?>
         <label class="checkbox" for=""><input name="admin[]" value="<?=$admin['id']?>" type="checkbox"><?=$admin['user_name']?></label>&nbsp;&nbsp;&nbsp;
